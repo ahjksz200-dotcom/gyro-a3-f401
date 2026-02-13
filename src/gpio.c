c9b873199b5465e37f83fbf61b2ba8e2b2895dd5
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-02-13 15:52:51 +0700

    Update gpio.c

--- src/gpio.c
+++ src/gpio.c
@@ -4,4 +4,13 @@
 void GPIO_Init(void)
 {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+
+    GPIOA->MODER &= ~(3 << (4 * 2));
+    GPIOA->MODER |=  (1 << (4 * 2));
+
+    GPIOA->OTYPER &= ~(1 << 4);
+    GPIOA->OSPEEDR |= (3 << (4 * 2));
+    GPIOA->PUPDR &= ~(3 << (4 * 2));
+
+    GPIOA->BSRR = (1 << 4);
 }
