cffe442b54c7e23039eacf36bce04c8dface3219
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-02-13 15:13:43 +0700

    Update mpu6500.h

--- include/mpu6500.h
+++ include/mpu6500.h
@@ -1,10 +1,13 @@
 #ifndef MPU6500_H
 #define MPU6500_H
 
+#include "stm32f4xx.h"
 #include <stdint.h>
 
 void MPU6500_Init(void);
-void MPU6500_Read(void);
+uint8_t MPU6500_ReadReg(uint8_t reg);
+void MPU6500_WriteReg(uint8_t reg, uint8_t data);
+void MPU6500_ReadGyro(void);
 
 extern float gyro_x;
 extern float gyro_y;
