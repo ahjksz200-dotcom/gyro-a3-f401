f0c56d39c78bbafef25a63f2992fb318b27cc0be
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-02-13 15:38:04 +0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,31 +1,33 @@
 #include "stm32f4xx.h"
-#include "spi.h"
-#include "mpu6500.h"
+#include  "spi.h"
+#include  "mpu6500.h"
+#include "gpio.h"
+#include "delay.h"
 
-void GPIO_Init(void);
-void delay(volatile uint32_t time);
+void GPIO_Init ( void ) ;
+void delay ( volatile uint32_t time ) ;
 
-int main(void)
+int main ( void )
 {
-    SystemInit();
+    SystemInit ( ) ;
 
-    GPIO_Init();
-    SPI1_Init();
-    MPU6500_Init();
+    GPIO_Init ( ) ;
+    SPI1_Init ( ) ;
+    MPU6500_Init ( ) ;
 
-    // Test WHO_AM_I
-    uint8_t id = MPU6500_ReadReg(0x75);
+    // Kiểm tra WHO_AM_I
+    uint8_t id = MPU6500_ReadReg ( 0x75 ) ;
 
     // Nếu đúng MPU6500 thì id = 0x70
-    while (1)
+    trong khi  ( 1 )
     {
-        MPU6500_ReadGyro();
+        MPU6500_ReadGyro ( ) ;
 
         // breakpoint tại đây để xem:
         // gyro_x
         // gyro_y
         // gyro_z
 
-        delay(50000);
+        trì hoãn ( 50000 ) ;
     }
 }
