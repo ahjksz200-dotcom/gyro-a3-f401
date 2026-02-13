ca61762d1b294eaa986d93ed2d0526a7b959a6c1
ahjksz200-dotcom <ahjksz200@gmail.com>
2026-02-13 13:44:09 +0700

    Create config.h

---
+++ include/config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define SERVO_MIN 1000
+#define SERVO_MAX 2000
+#define SERVO_MID 1500
+
+#define PID_P 3.5f
+#define PID_I 0.02f
+#define PID_D 15.0f
+
+#endif
